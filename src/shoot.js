d52d6024e9a57abbcbdbddc30fee731f71cc939a
huweixuan <weixuan.hu@dianping.com>
2015-06-18 11:55:19 +0800

    purecss

--- src/shoot.js
+++ src/shoot.js
@@ -1,8 +1,8 @@
 (function ($) {
   $.shoot = function(element, options) {
     var defaults = {
-      vertex_top: 20, // 默认顶点高度
-      speed: 1.2,
+      vertex_height: 120, // 默认抛物线高度
+      duration: 0.5,
       start: {}, // top, left
       end: {},
       onEnd: $.noop
@@ -18,54 +18,56 @@
 
     self.setOptions = function(options) {
       this.settings = $.extend(true, {}, defaults, options);
-
-      var settings = this.settings,
-        speed = settings.speed,
-        vertex_top = settings.vertex_top,
-        start = settings.start,
-        end = settings.end;
-
-      $element.appendTo('body').css({
-        'transition': 'all',
-        'position': 'absolute'  
-      });
-
-      var distance = Math.sqrt(Math.pow(start.top - end.top, 2) + Math.pow(start.left - end.left, 2)),
-        steps = Math.ceil(Math.min(Math.max(Math.log(distance) / 0.05 - 75, 30), 100) / settings.speed),
-        ratio = start.top == vertex_top ? 0 : -Math.sqrt((end.top - vertex_top) / (start.top - vertex_top)),
-        vertex_left = (ratio * start.left - end.left) / (ratio - 1),
-        curvature = end.left == vertex_left ? 0 : (end.top - vertex_top) / Math.pow(end.left - vertex_left, 2);
-
-        $.extend(true, settings, {
-          count: -1,
-          steps: steps,
-          vertex_left: vertex_left,
-          vertex_top: vertex_top,
-          curvature: curvature
-        });
     };
 
     self.move = function() {
       var settings = this.settings,
+        duration = settings.duration,
+        vertex_height = settings.vertex_height,
         start = settings.start,
-        count = settings.count,
-        steps = settings.steps,
         end = settings.end;
 
-      var left = start.left + (end.left - start.left) * count / steps,
-        top = settings.curvature == 0 ? start.top + (end.top - start.top) * count / steps : settings.curvature * Math.pow(left - settings.vertex_left, 2) + settings.vertex_top,
-        deg = 720 * count / steps;
-
-      settings.count++;
-
-      $element.css({
-        'transform': 'translate(' + left + 'px, ' + top + 'px) rotate(' + deg + 'deg)'
+      $element.appendTo('body').css({
+        'position': 'absolute',
+        'top': start.top,
+        'left': start.left   
       });
+      window.requestAnimationFrame(moveUp);
+      function moveUp() {  
+        $element.css({
+          'transition': duration / 2 + 's left linear, ' + duration / 2 + 's top ease-out, ' + duration / 2 + 's transform ease',
+          'top': start.top - vertex_height,
+          'left': Math.abs(end.left - start.left) / 2,
+          'transform': 'rotate(360deg)'
+        }).on('transitionend', moveDown);
+      }
 
-      var time = window.requestAnimationFrame($.proxy(this.move, this));
-      if (count == steps) {
-        window.cancelAnimationFrame(time);
-        settings.onEnd.apply(this);
+      function moveDown() {
+        if(animateNotComplete()){
+          return;
+        }
+        $element.css({
+          'transition': duration / 2 + 's left linear, ' + duration / 2 + 's top ease-in, ' + duration / 2 + 's transform ease',
+          'top': end.top,
+          'left': end.left,
+          'transform': 'rotate(720deg)'
+        }).on('transitionend', moveEnd);
+      }
+      function moveEnd() {
+        if(animateNotComplete()){
+          return;
+        }
+        settings.onEnd.apply(self);
+      }
+      var _count = 0;
+      function animateNotComplete() {
+        //每个属性的变动都会触发transitionend
+        _count++;
+        if(_count < 3) {
+          return true;
+        }
+        _count = 0;
+        return false;
       }
     };
 
